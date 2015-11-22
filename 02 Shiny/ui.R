97f914c8a11f8e14de8de27212d6740c8ebcf045
pn4322 <prarthana_neotia@hotmail.com>
2015-11-22 17:14:15 -0600

    no message

--- 02 Shiny/ui.R
+++ 02 Shiny/ui.R
@@ -2,53 +2,39 @@
 
 library(shiny)
 
-# Define UI for application that plots random distributions 
-shinyUI(pageWithSidebar(
-
-# Application title
-headerPanel("Hello Shiny!"),
-
-# Sidebar with a slider input for number of observations
-  sidebarPanel(
-    
-    sliderInput("KPI1", 
-                "KPI_Low_Max_value:", 
-                min = 0,
-                max = 0.05, 
-                value = 0.05),
-    sliderInput("KPI2", 
-                "KPI_Medium_Max_value:", 
-                min = 0.05,
-                max = 0.14, 
-                value = 0.14),
-    sliderInput("LowerLimit", 
-                "Lower_Limit", 
-                min = 0,
-                max = 0.9917225, 
-                value = 0.9917225),
-    
-    sliderInput("UpperLimit", 
-                "Upper_Limit", 
-                min = 0.9917225,
-                max = 1.04, 
-                value = 0.9961),
-    
-    sliderInput("Quality",
-                "Quality_Range",
-                min=4,
-                max=8,
-                value=6),
-    sliderInput("AVG_ALCOHOL",
-                "average_alcohol_range",
-                min=1,
-                max=13.2,
-                value=6)
-    
+navbarPage(
+  title = "Elements of Visualization",
+  tabPanel(title = "Crosstab",
+           sidebarPanel(
+             actionButton(inputId = "light", label = "Light"),
+             actionButton(inputId = "dark", label = "Dark"),
+             sliderInput("KPI1", "KPI_Low_Max_value:", 
+                         min = 0, max = 0.05,  value = 0.05),
+             sliderInput("KPI2", "KPI_Medium_Max_value:", 
+                         min = 0.05, max = 0.14,  value = 0.14),
+             textInput(inputId = "title", 
+                       label = "Crosstab Title",
+                       value = "Wine Crosstab\nSUM_CITRIC_ACID, SUM_RESIDUAL_SUGAR, SUM_CITRIC_ACID / SUM_RESIDUAL_SUGAR"),
+             actionButton(inputId = "clicks1",  label = "Click me")
+           ),
+           
+           mainPanel(plotOutput("distPlot1")
+           )
   ),
-
-# Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot")
-    #plotOutput("distTable")
+  tabPanel(title = "Barchart",
+           sidebarPanel(
+             actionButton(inputId = "clicks2",  label = "Click me")
+           ),
+           
+           mainPanel(plotOutput("distPlot2")
+           )
+  ),
+  tabPanel(title = "Scatterplot",
+           sidebarPanel(
+             actionButton(inputId = "clicks3",  label = "Click me")
+           ),
+           
+           mainPanel(plotOutput("distPlot3")
+           )        
   )
-))
+)
